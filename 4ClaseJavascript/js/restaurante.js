f727e34dfe9337aedade198d074950982dcb4029
YencyKarinaRodriguez <129075276+YencyKarinaRodriguez@users.noreply.github.com>
2023-11-30 21:33:48 -0500

    ..

--- 4ClaseJavascript/js/restaurante.js
+++ 4ClaseJavascript/js/restaurante.js
@@ -1,45 +1,20 @@
-var resultado=document.getElementById('resultado');
+function factura (){
 
-//aqui se crea la funcion de calcular pedido
-function calcularTotal(cantidad, valor) {
-    return cantidad * valor;
-}
+    // Tomar el valor de la cantidad de platos
+    var canPlato = parseInt(document.getElementById("canPlato").value)
+    // Tomar el valor del plato
+    var valPlato = parseInt(document.getElementById("valPlato").value)
+    // Tomar del tipo de plato
+    var tipoPlato = document.getElementById("tipoPlato").value
+    // Calcular el total del pedido
+    total = canPlato * valPlato;
 
-//aqui se crea la funcion para mostrar el mensaje
-function mostrarResultado(mensaje) {
-
-    let resultadoElemento = documento.getElementById("resultado");
-    let respuestaElemento = documento.getElementById("respuesta");
-
-    respuestaElemento.estilo.display = "bloque";
-
-    //aqui se muestra el mensaje
-    resultadoElemento.innerHTML = mensaje;
-
-    //aqui se establece el color del texto en verde si es un pedido exitoso, rojo si hay un error
-    if (mensaje.includes("Pedido realizado")) {
-        resultadoElemento.estilo.color = "verde";
-    } else {
-        resultadoElemento.estilo.color = "rojo";
-    }
-}
-
-//aqui se crea ka funcion para realizar la orden
-function mostrarOrden() {
-    //aqui se muestra el valor de los platos, cantidad o tipo
-    const cantidad = parseInt(documento.getElementById("cantidad").valor);
-    const valor = parseInt(document.getElementById("valor").valor);
-    const tipo = documento.getElementById("tipo").valor;
-
-    if (cantidad  > 0 && valor > 0 &&  tipo !== "Tipo de Plato") {
-        //aqui se calcula el total del pedido
-        let total = calcularTotal(cantidad, valor);
-        //aqui se muestra el total del pedido
-        mostrarResultado('Pedido realizado: ${cantidad} platos de  ${tipo}. Total a pagar: $${total}');
-    } else {
-        //aqui se muestra un mensaje de algun error
-        mostrarResultado("Verifique sus datos. Complete la cantidad, valor y tipo de plato correctamente.");
-    }
-
-    resultado.innerHTML= total; 
+    // Mostrar el cuadro oculto cuando se de el resultado final
+    var res = document.getElementById("total")
+        // Cambiar estado de none a block
+        respuesta.style.display = "block"
+        // Texto a mostrar en pantalla
+        res.innerHTML = `Pedido realizado: \$${canPlato} platos de \$${tipoPlato}. Total a pagar: $${total}`;
+        // Texto en color verde
+        res.style.color = "green";
 }
